b2799853ef7a84567455ff3a2524e593f313d3fb
George Dickinson <georgedickinson5291@outlook.com>
2024-07-05 16:20:15 +0100

    Initial grid that can be set by user prompt
    -Created a grid that is sized based on user input
    -Used HTML, CSS & JS to create this

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-const div = document.createElement("div");
-document.body.appendChild(div);
+let userNumber = prompt("What would you like the grid size to be?")
+let gridSize = userNumber ** 2;
+
+const gridContainer = document.querySelector(".grid-container");
+
+for (i = 0; i < gridSize; i++) {
+    const div = document.createElement("div");
+    div.style.flexBasis = `calc(${100 / userNumber}%)`;
+    div.style.height = `calc(${100 / userNumber}%)`;
+    gridContainer.appendChild(div);
+}
